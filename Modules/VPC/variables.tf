960dcc50f8466e8ead667d0a32d9cc0267be7519
Juan Catalan <jcatalanolmos@gmail.com>
2017-02-19 17:06:17 -0300

    generic amazon AMI and Nat image are now a map based on region

--- Modules/VPC/variables.tf
+++ Modules/VPC/variables.tf
@@ -6,21 +6,16 @@ variable "pem_path" {}
 variable "cidr_block" {}
 variable "proj_name" {}
 
-variable "cidr_block_sub" {
-   default = {
-      "0"  = "10.0.0.0/24"
-      "1"  = "10.0.1.0/24"
-      "2"  = "10.0.2.0/24"
-      "100"  = "10.0.100.0/24"
-      "101"  = "10.0.101.0/24"
-      "102"  = "10.0.102.0/24"
-   }
-}
-
 variable "NAT_AMI" {
-	default = "ami-67a54423"
+	default = {
+		"us-weast-1" = "ami-67a54423"
+		"sa-east-1"  = "ami-93fb408e"
+	}
 }
 
 variable "amzn_AMI" {
-  default = "ami-31490d51"
+  default = {
+	"us-weast-1" = "ami-31490d51"
+        "sa-east-1"  = "ami-80086dec"
+ }
 }
