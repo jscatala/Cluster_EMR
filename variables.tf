3bce85257db376d856f4185e1850eda7b5414118
Juan Catalan <jcatalanolmos@gmail.com>
2017-02-19 16:59:28 -0300

    az now are a map instead of a csv list

--- variables.tf
+++ variables.tf
@@ -3,7 +3,12 @@ variable "aws_region" {
 }
 
 variable "av_zones" {
-	default = "sa-east-1a,sa-east-1b,sa-east1c"
+    default = {
+        "us-west-1" = "us-west-1b,us-west-1c"
+        "us-west-2" = "us-west-2a,us-west-2b,us-west-2c"
+        "us-east-1" = "us-east-1c,us-west-1d,us-west-1e"
+	"sa-east-1" = "sa-east-1a,sa-east-1b,sa-east-1c"
+    }
 }
 
 variable "cidr_block" {
