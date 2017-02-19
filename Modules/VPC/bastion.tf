8d682df9e4d3239ef063b59333242d66ff1bb84d
Juan Catalan <jcatalanolmos@gmail.com>
2017-02-19 17:26:00 -0300

    added bastion config

--- Modules/VPC/bastion.tf
+++ Modules/VPC/bastion.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ssh" {
     ami                         = "${var.amzn_AMI}"
     availability_zone           =  "${element(split(",", var.av_zones), 1)}"
-    instance_type               = "t2.micro"
+    instance_type               = "t2.micro"  # instance type has to be specified 
     key_name                    = "${var.pem}"
     vpc_security_group_ids      = ["${aws_security_group.ssh.id}"] 
     subnet_id                   = "${aws_subnet.public_1b.id}"
@@ -26,26 +26,5 @@ resource "aws_instance" "ssh" {
       "sudo yum update -y",
     ]
   }
-
-    # Install Mysql 
-    provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      "sudo yum install mysql -y",
-    ]
-  }
-  
-  //Find a way to provide specifically for each environment
-  #copy ssh keys
-  provisioner "file" {
-    source      = "${var.pem_path}/Production.pem"
-    destination = "/home/ec2-user/Production.pem"
-  }
-
-  provisioner "file" {
-    source      = "${var.pem_path}/Stage.pem"
-    destination = "/home/ec2-user/Stage.pem"
-  }
-
 
 }
