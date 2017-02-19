f6a6e6e37157d8edee91c3ce3dca0bdb8df829e7
Juan Catalan <jcatalanolmos@gmail.com>
2017-02-19 16:58:36 -0300

    az lookup done when calling the module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "Network" {
    source     = "Modules/VPC"
    ts         = "${var.timestamp}"
    region     = "${var.aws_region}"
-   av_zones   = "${var.av_zones}"
+   av_zones   = "${lookup(var.av_zones, var.aws_region)}"
    cidr_block = "${var.cidr_block}"
    pem        = "${var.aws_pem}"
    pem_path   = "${var.pem_folder_path}"
